764cdbc9e180ff4adcf53dbd78719c7e0d94a07f
Timmay24 <tim.hartig@haw-hamburg.de>
2014-11-02 19:10:00 +0100

    Added Badge class.

--- SoLe/src/edu/haw/se1/sole/belohnungssystem/Badge.java
+++ SoLe/src/edu/haw/se1/sole/belohnungssystem/Badge.java
@@ -1,9 +1,41 @@
 package edu.haw.se1.sole.belohnungssystem;
 
 public class Badge {
+	
+	private String name;
+	private String beschreibung;
+	private String bildPfad;
 
-	public Badge() {
-		// TODO Auto-generated constructor stub
+	public Badge(String name, String beschreibung, String bildPfad) {
+		this.setName(name);
+		this.setBeschreibung(beschreibung);
+		this.setBildPfad(bildPfad);
 	}
 
+	public String getName() {
+		return name;
+	}
+	
+	private void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getBeschreibung() {
+		return beschreibung;
+	}
+
+	private void setBeschreibung(String beschreibung) {
+		this.beschreibung = beschreibung;
+	}
+
+	public String getBildPfad() {
+		return bildPfad;
+	}
+
+	private void setBildPfad(String bildPfad) {
+		this.bildPfad = bildPfad;
+	}
+
+	
+	
 }
