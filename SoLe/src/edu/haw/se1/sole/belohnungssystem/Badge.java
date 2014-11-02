24c7e22c3c8d416ef48f51eef8f26e1c0f38f1f4
Timmay24 <tim.hartig@haw-hamburg.de>
2014-11-02 19:28:50 +0100

    Badge: equals and hashcode implemented Belohnungssystem: createBadge and getBadge implemented. DB simulation through holding data sets in private HashMap badges.

--- SoLe/src/edu/haw/se1/sole/belohnungssystem/Badge.java
+++ SoLe/src/edu/haw/se1/sole/belohnungssystem/Badge.java
@@ -36,6 +36,37 @@ public class Badge {
 		this.bildPfad = bildPfad;
 	}
 
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Badge))
+			return false;
+		Badge other = (Badge) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
 	
 	
 }
--- SoLe/src/edu/haw/se1/sole/belohnungssystem/Belohnungssystem.java
+++ SoLe/src/edu/haw/se1/sole/belohnungssystem/Belohnungssystem.java
@@ -1,11 +1,30 @@
 package edu.haw.se1.sole.belohnungssystem;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import edu.haw.se1.sole.IPersistenceServices;
 
 public class Belohnungssystem implements IBelohnungssystem, IPersistenceServices {
+	
+	private Map<String, Badge> badges;
 
 	public Belohnungssystem() {
-		// TODO Auto-generated constructor stub
+		this.badges = new HashMap<>();
 	}
+	
+	public Badge createBadge(String name) {
+		if (name == null) return null;
+		
+		Badge newBadge = new Badge(name, "beschreibungstext", "bildPfad");
+		badges.put(name, newBadge);
+		return newBadge;
+	}
+	
+	public Badge getBadge(String name) {
+		return badges.get(name); 
+	}
+	
+	
 
 }
