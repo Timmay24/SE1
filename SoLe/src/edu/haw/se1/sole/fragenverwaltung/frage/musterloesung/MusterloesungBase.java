301f48d227b56306e77b673f527b68225384c5b2
Timmay24 <tim.hartig@haw-hamburg.de>
2014-11-04 23:17:27 +0100

    removed private setter.

--- SoLe/src/edu/haw/se1/sole/fragenverwaltung/frage/musterloesung/MusterloesungBase.java
+++ SoLe/src/edu/haw/se1/sole/fragenverwaltung/frage/musterloesung/MusterloesungBase.java
@@ -5,7 +5,6 @@ import edu.haw.se1.sole.fragenverwaltung.IMusterloesung;
 public abstract class MusterloesungBase implements IMusterloesung {
 	
 
-	protected MusterloesungBase() {
-	}
+	protected MusterloesungBase() {}
 
 }
