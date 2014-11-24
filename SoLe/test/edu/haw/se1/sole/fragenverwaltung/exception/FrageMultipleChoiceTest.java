0e3867bd5c5d277892275f4dece80a1470cce06d
Timmay24 <tim.hartig@haw-hamburg.de>
2014-11-24 18:25:38 +0100

    Corrected calling of DependencyBuilder for dummy purposes.

--- SoLe/test/edu/haw/se1/sole/fragenverwaltung/exception/FrageMultipleChoiceTest.java
+++ SoLe/test/edu/haw/se1/sole/fragenverwaltung/exception/FrageMultipleChoiceTest.java
@@ -25,7 +25,7 @@ public class FrageMultipleChoiceTest {
     @Before
     public void setUp() {
         da = new DependencyAssembler();
-		da.buildDependencies();
+		da.buildTestDependencies();
 		fragenVerwaltung = da.getFragenVerwaltung();
 		modulVerwaltung = da.getModulVerwaltung();
     }
